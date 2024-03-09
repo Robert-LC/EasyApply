24d272a0fd595cc4f1182299f3a9b957b9d92b9e
Robert Locicero <robert_lc1@protonmail.com>
2024-03-08 20:59:06 -0500

    Create initial styling

--- easy-apply/src/app/page.tsx
+++ easy-apply/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import React from "react";
 import styles from "./page.module.css";
+import EntriesList from "./components/EntriesList/entriesList";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <EntriesList />
     </main>
   );
 }
