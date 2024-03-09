5a54d3ef30a92399fa9f247443ef6947ad6a60ea
Robert Locicero <robert_lc1@protonmail.com>
2024-03-09 15:10:14 -0500

    Add LinkedIn and Github icons to footer

--- easy-apply/src/app/components/Footer/footer.tsx
+++ easy-apply/src/app/components/Footer/footer.tsx
@@ -1,12 +1,21 @@
 "use client";
 import React from 'react';
 import styles from './footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
-            <div>
-                <h2>EasyApply by Robert Locicero</h2>
+            <h2>EasyApply by Robert Locicero</h2>
+            <div className={styles.icons}>
+                <a href="https://www.linkedin.com/in/robert-locicero/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} size="4x"/>
+                </a>
+                <a href="https://github.com/Robert-LC/EasyApply" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} size="4x" color="blank"/>
+                </a>
             </div>
         </footer>
     );
