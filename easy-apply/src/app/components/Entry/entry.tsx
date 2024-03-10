a19602d09e8b36a6a56e66156bb31ac07495f1f4
Robert Locicero <robert_lc1@protonmail.com>
2024-03-09 23:51:58 -0500

    Only show remove button on hover

--- easy-apply/src/app/components/Entry/entry.tsx
+++ easy-apply/src/app/components/Entry/entry.tsx
@@ -13,15 +13,20 @@ interface EntryProps {
 
 const Entry: React.FC<EntryProps> = ({ id, title }) => {
     const [textValue, setTextValue] = React.useState('');
+    const [isHovered, setIsHovered] = React.useState(false);
 
     return (
-        <div id={`Entry-${id}`} className={styles.entry}>
+        <div id={`Entry-${id}`} className={styles.entry}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             <div className={styles.textInput}>
                 <TitleTextBox title={title} />
                 <EntryTextBox textValue={textValue} setTextValue={setTextValue}/>
             </div>
             <div className={styles.buttonContainer}>
-                <RemoveEntryButton />
+                <div style={{ visibility: isHovered ? 'visible' : 'hidden' }}>
+                    <RemoveEntryButton />
+                </div>
                 <ClipboardButton textValue={textValue} />
             </div> 
         </div>
