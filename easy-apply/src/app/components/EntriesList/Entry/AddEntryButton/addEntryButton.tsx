e8183a6c05caf31c6986dc6248f8dc79c99aadf3
Robert Locicero <robert_lc1@protonmail.com>
2024-03-12 22:17:14 -0400

    Create AddEntryButton functionality

--- easy-apply/src/app/components/EntriesList/Entry/AddEntryButton/addEntryButton.tsx
+++ easy-apply/src/app/components/EntriesList/Entry/AddEntryButton/addEntryButton.tsx
@@ -1,14 +1,18 @@
-"use client";
 import React from 'react';
 import styles from './addEntryButton.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const AddEntryButton: React.FC = () => {
+interface AddEntryButtonProps {
+    onClick: () => void;
+}
 
+const AddEntryButton: React.FC<AddEntryButtonProps> = ({ onClick }) => {
+
+    
     return (
         <div className={styles.addEntryButtonContainer}>
-            <button className={styles.addEntryButton}>
+            <button className={styles.addEntryButton} onClick={onClick}>
                 <FontAwesomeIcon icon={faPlus} size='2x' />
             </button>
         </div>
--- easy-apply/src/app/components/EntriesList/entriesList.tsx
+++ easy-apply/src/app/components/EntriesList/entriesList.tsx
@@ -1,20 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { EditContext } from '@/contexts/editContext';
 import Entry from './Entry/entry';
 import styles from './entriesList.module.css';
-import entries from '../../defaultEntries.json';
+import defaultEntries from '../../defaultEntries.json';
 import AddEntryButton from './Entry/AddEntryButton/addEntryButton';
 
 const EntriesList: React.FC = () => {
     const { isEditing } = useContext(EditContext);
+    const [entryList, setEntryList] = useState(defaultEntries);
+
+    const addNewEntry = () => {
+        const newEntry = {
+            id: entryList.length + 1,
+            text: "New Entry"
+        };
+        setEntryList([...entryList, newEntry]);
+    };
+
     return (
         <div className={styles.entriesList}>
-            {entries.map((entry) => (
+            {entryList.map((entry) => (
                 <Entry key={entry.id} id={entry.id.toString()} title={entry.text}/>
             ))}
-            {isEditing && <AddEntryButton />}
+            {isEditing && <AddEntryButton onClick={addNewEntry} />}
         </div>
     );
 };
+
 
 export default EntriesList;
