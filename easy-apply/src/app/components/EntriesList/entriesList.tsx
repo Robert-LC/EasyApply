affc3080fdf94efc2a7afbd59f815ddc9235c754
Robert Locicero <robert_lc1@protonmail.com>
2024-03-12 21:49:26 -0400

    Create, style and place AddEntryButton

--- easy-apply/src/app/components/EntriesList/entriesList.tsx
+++ easy-apply/src/app/components/EntriesList/entriesList.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { EditContext } from '@/contexts/editContext';
 import Entry from './Entry/entry';
 import styles from './entriesList.module.css';
 import entries from '../../defaultEntries.json';
+import AddEntryButton from './Entry/AddEntryButton/addEntryButton';
 
 const EntriesList: React.FC = () => {
+    const { isEditing } = useContext(EditContext);
     return (
         <div className={styles.entriesList}>
             {entries.map((entry) => (
                 <Entry key={entry.id} id={entry.id.toString()} title={entry.text}/>
             ))}
+            {isEditing && <AddEntryButton />}
         </div>
     );
 };
