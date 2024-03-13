77811fbcbd9a885370a6fa6c8220eae0cb38092e
Robert Locicero <robert_lc1@protonmail.com>
2024-03-12 21:30:24 -0400

    Create EditContext

---
+++ easy-apply/src/contexts/editContext.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { Dispatch, SetStateAction } from "react";
+import { createContext, useState } from "react";
+
+interface EditContextInterface {
+    isEditing: boolean;
+    setIsEditing: Dispatch<SetStateAction<boolean>>;
+}
+
+export const EditContext = createContext<EditContextInterface>({ 
+    isEditing: false, setIsEditing: (isEditing) => {} 
+});
+
+type EditContextProviderProps = {
+    children: React.ReactNode;
+}
+
+export default function EditContextProvider({ children }: EditContextProviderProps) {
+    const [isEditing, setIsEditing] = useState(false);
+    return (
+        <EditContext.Provider value={{ isEditing, setIsEditing }}>
+            {children}
+        </EditContext.Provider>
+    );
+}
